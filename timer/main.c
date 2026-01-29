8e5b0cae3059f1711b6f532f15c6bded302957f3
Spencer Huang <spencer.y.huang@gmail.com>
2026-01-28 22:36:17 -0800

    Set up vendor files, Makefile

---
+++ timer/main.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include "stm32g0xx.h"
+
+#define LED_PIN 5
+
+void main(void) {
+	// enable GPIO_A clock
+	RCC->IOPENR |= (1 << RCC_IOPENR_GPIOAEN_Pos);
+
+	// dummy reads
+	volatile uint32_t* dummy;
+	dummy = RCC->IOPENR;
+	dummy = RCC->IOPENR;
+
+	// Reset the mode of PA5
+	GPIOA->MODER &= ~(0x3 << GPIO_MODER_MODE5_Pos);
+	// Set PA5 to output
+	GPIOA->MODER |= (0x1 << GPIO_MODER_MODE5_Pos);
+
+	while(1) {
+		// Toggle PA5.
+		GPIOA->ODR ^= (1 << LED_PIN);
+		for(uint32_t i = 0; i < 1000000; i++);
+	}
+}
