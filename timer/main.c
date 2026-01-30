bb25f0034017f436f5988b201782bc5ea5e18802
Spencer Huang <spencer.y.huang@gmail.com>
2026-01-29 23:31:18 -0800

    added button input - but next step would be to implement button input via interrupts + debouncing

--- timer/main.c
+++ timer/main.c
@@ -13,11 +13,11 @@ void main(void) {
 	NVIC_SetPriority(TIM14_IRQn, 0x03);
 	NVIC_EnableIRQ(TIM14_IRQn);
 
-
-	// enable GPIO_A clock
+	// enable GPIO_A, GPIO_C clock
 	RCC->IOPENR |= (1 << RCC_IOPENR_GPIOAEN_Pos);
+	RCC->IOPENR |= (1 << RCC_IOPENR_GPIOCEN_Pos);
 
-	// dummy reads
+	// dummy reads - this required by errata, but that was for a different board
 	volatile uint32_t* dummy;
 	dummy = RCC->IOPENR;
 	dummy = RCC->IOPENR;
@@ -27,9 +27,27 @@ void main(void) {
 	// Set PA5 to output
 	GPIOA->MODER |= (0x1 << GPIO_MODER_MODE5_Pos);
 
+	// Reset the mode of PC13 (this also sets the mode to input)
+	GPIOC->MODER &= ~(0x3 << GPIO_MODER_MODE13_Pos);
+	// Set PC13 to pull-up
+	GPIOC->PUPDR |= (0x1 << GPIO_MODER_MODE13_Pos);
+
 	start_timer();
 
-	while(1) { }
+	int prev_pressed = 0;
+	while(1) { 
+		// while the button is depressed - the timer will blink four times as fast
+		int pressed = ~(GPIOC->IDR >> GPIO_IDR_ID13_Pos) & 1;
+		if (prev_pressed != pressed) {
+			prev_pressed = pressed;
+			if (pressed) {
+				TIM14->ARR = 250; // 0.25 seconds
+			} else {
+				TIM14->ARR = 1000; // 1 second
+			}
+			TIM14->EGR |= TIM_EGR_UG;
+		}
+	}
 }
 
 // for now, hardcoded to use TIM14
@@ -42,7 +60,7 @@ void start_timer(void) {
 	RCC->APBRSTR2 &= ~(RCC_APBRSTR2_TIM14RST);
 
 	// set prescaler/autoreload registers
-	TIM14->PSC = 16000000/1000;
+	TIM14->PSC = 16000000/1000; // defaults to HSI which is 16 MHz
 	TIM14->ARR = 1000; // 1 second
 
 	// set update event to reset timer
